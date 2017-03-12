6a100800079de7f6117dcbc75b3665447ab2fe03
Narek <narek.kirakossian@gmail.com>
2017-03-12 02:24:31 +0000

    init devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
           :rememberable,
           :trackable,
           :validatable
+          # :confirmable
           # :omniauthable,
           # :omniauth_providers => [:google_oauth2]
 end
