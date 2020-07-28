b5fd9da34588726337803b23cdc77522debe2365
Narek <narek.kirakossian@gmail.com>
2020-07-28 12:44:26 -0600

    add some seed data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,13 @@
 module ApplicationHelper
-  
-  
+  require "faker"
+
   def profile_img
     # current_user.profile_img if defined? current_user.profile_img
     # else
-     profile_img = "https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"
+    profile_img = "https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"
   end
-  
-  
-  
 
-  def full_title(page_title = '')
+  def full_title(page_title = "")
     base_title = "Castme.com"
     if page_title.empty?
       base_title
@@ -18,22 +15,22 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-  
+
   def bootstrap_class_for(flash_type)
     case flash_type
-      when "success"
-        "alert-success"   # Green
-      when "error"
-        "alert-danger"    # Red
-      when "alert"
-        "alert-warning"   # Yellow
-      when "notice"
-        "alert-info"      # Blue
-      else
-        flash_type.to_s
+    when "success"
+      "alert-success"   # Green
+    when "error"
+      "alert-danger"    # Red
+    when "alert"
+      "alert-warning"   # Yellow
+    when "notice"
+      "alert-info"      # Blue
+    else
+      flash_type.to_s
     end
   end
-  
+
   def devise_error_messages!
     return "" unless devise_error_messages?
 
@@ -55,12 +52,13 @@ module ApplicationHelper
   def devise_error_messages?
     !resource.errors.empty?
   end
-  
+
+  # Check if castingcalls have main image
   def check_img(main_image)
     if main_image.present?
       main_image
     else
-      "https://via.placeholder.com/1500"
+      Faker::LoremPixel.image(size: "900x800", is_gray: false, category: "nature")
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ApplicationRecord
   has_many :castingcalls, dependent: :destroy
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise  :database_authenticatable, :registerable,:recoverable, :rememberable,
-          :trackable, :validatable, # :confirmable
-          :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :trackable, :validatable, # :confirmable
+         :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.first_name 
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.first_name
       # user.surname = auth.info.last_name
       # user.profile_img = auth.info.image # assuming the user model has an image
     end
